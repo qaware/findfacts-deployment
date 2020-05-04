45ef0863c837afcff8952306709b5eae3d6b3ce4
Fabian Huch <fabian@huch.de>
2020-05-04 21:32:30 +0200

    Bumped version + renamed container

--- update-deployed.sh
+++ update-deployed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replacement variables
-export VERSION="0.4.1" SCHEMA_VERSION="0.3.1"
+export VERSION="0.4.2" SCHEMA_VERSION="0.3.1"
 
 shopt -s nullglob dotglob
 root=$(dirname "$BASH_SOURCE")
